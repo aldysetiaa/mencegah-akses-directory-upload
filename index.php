f7150eb2ae64482a7f97c41c17ba223cac3cf7f2
Aldy Setiawan <70496872+aldysetiaa@users.noreply.github.com>
2022-04-15 22:00:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <h3>Lihat File</h3>
 <ul>
 	<li><a href='preview/<?php echo base64_encode('c1.jpg'); ?>'>Foto</a></li>
-	<li><a href='preview/<?php echo base64_encode('c2.pdf'); ?>'>PDF</a></li> 
+	<li><a href='preview/<?php echo base64_encode('Pentesting-With-Burp-Suite.pdf'); ?>'>PDF</a></li> 
 </ul>
 </body>
 </html>
